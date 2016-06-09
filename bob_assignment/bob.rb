ff38fdab20806fa5fecd1bc5c1363edafe4a37cf
Brian Simmons <bsimmonsn@gmail.com>
2016-06-09 10:47:21 -0500

    cleaned up code a bit

--- bob_assignment/bob.rb
+++ bob_assignment/bob.rb
@@ -1,7 +1,7 @@
 class Bob
 
 def has_digits(remark)
-  if remark =~ /\d+/  #I got the /\d+/ off of Rubyquicktips.com, I understand it means digits.
+  if remark =~ /\d+/  #I got the /\d+/ regex off of Rubyquicktips.com, I understand it means digits.
     return true
   else
     return false
@@ -15,36 +15,29 @@ def only_spaces(remark)
       return true
     else
       return false
+    end
   end
-end
 end
 
 
 def hey(remark)
   case
-  when remark.empty? || only_spaces(remark) || remark.include?("\t")
-    "Fine. Be that way!"
-  when has_digits(remark) && remark.end_with?("?") && remark.length <4
-    "Sure."
-  when remark.upcase == remark && has_digits(remark) && remark.end_with?("!")
-    "Whoa, chill out!"
-  when remark.upcase == remark && has_digits(remark)
+    when remark.empty? || only_spaces(remark) || remark.include?("\t")
+      "Fine. Be that way!"
+    when has_digits(remark) && remark.end_with?("?") && remark.length <4
+      "Sure."
+    when remark.upcase == remark && has_digits(remark) && remark.end_with?("!")
+      "Whoa, chill out!"
+    when remark.upcase == remark && has_digits(remark)
+      "Whatever."
+    when remark.upcase == remark
+      "Whoa, chill out!"
+    when remark.end_with?("?")
+      "Sure."
+    else
     "Whatever."
-  when remark.upcase == remark
-    "Whoa, chill out!"
-
-
-
-
-  when remark.end_with?("?")
-    "Sure."
-
-
-
-  else
-  "Whatever."
+    end
   end
-end
 
 end
 
